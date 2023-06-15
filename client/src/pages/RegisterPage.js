d0e7ae1ededb99e1a7ea3cf8bedea8fc3d887cac
Major <107882188+qubitflip@users.noreply.github.com>
2023-06-15 12:56:50 -0500

    JSON server response listener

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function register(e) {
+        e.preventDefault();
+        await fetch("http://localhost:4000/register", {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type': 'application/json'}
+        })
+    }
+
     return (
         <div className="forms">
             <div className="form-header">Register</div>
-            <form className="register">
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
+            <form className="register" onSubmit={register}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button>Register</button>
             </form>
         </div>
