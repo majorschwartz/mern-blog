0a11b0aabe99e4fd54e3fc1bddb29e107b580233
Major <107882188+qubitflip@users.noreply.github.com>
2023-06-15 10:49:11 -0500

    Individualized components

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,14 @@
 import "./App.css";
+import Post from "./Post";
+import Header from "./Header";
 
 function App() {
     return (
         <main>
-            <header>
-                <a href="" className="logo">
-                    Blog
-                </a>
-                <nav>
-                    <a href="">Login</a>
-                    <a href="">Register</a>
-                </nav>
-            </header>
-
-            <div className="post">
-                <div className="image">
-                    <img
-                        src="https://www.writingbeginner.com/wp-content/uploads/2023/02/Cartoon-blogger-on-a-laptop-How-to-write-a-blog-post-with-ChatGPT.png"
-                        alt=""
-                    />
-                </div>
-                <div className="texts">
-                    <h2>How to write a blog post with ChatGPT</h2>
-                    <p className="info">
-                        <span className="author">Major</span>
-                        <span className="time">06-15-2023 10:38</span>
-                    </p>
-                    <p className="summary">
-                        Ever since it was released, I've been experimenting with
-                        writing content with ChatGPT. Using my prior knowledge
-                        and experience ranking articles written with the help of
-                        other premium AI tools (such as Jasper AI), I was able
-                        to develop a 21-step workflow.
-                    </p>
-                </div>
-            </div>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
         </main>
     );
 }
---
+++ client/src/Post.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Post = () => {
+    return (
+        <div className="post">
+            <div className="image">
+                <img
+                    src="https://www.writingbeginner.com/wp-content/uploads/2023/02/Cartoon-blogger-on-a-laptop-How-to-write-a-blog-post-with-ChatGPT.png"
+                    alt=""
+                />
+            </div>
+            <div className="texts">
+                <h2>How to write a blog post with ChatGPT</h2>
+                <p className="info">
+                    <span className="author">Major</span>
+                    <span className="time">06-15-2023 10:38</span>
+                </p>
+                <p className="summary">
+                    Ever since it was released, I've been experimenting with
+                    writing content with ChatGPT. Using my prior knowledge and
+                    experience ranking articles written with the help of other
+                    premium AI tools (such as Jasper AI), I was able to develop
+                    a 21-step workflow.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Post;
