9a0865f8b1d9aec9b847bd964bbf41268c1112a3
Major <107882188+qubitflip@users.noreply.github.com>
2023-06-15 11:13:24 -0500

    Login and Register page additions

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
-            <a href="" className="logo">
+            <Link to="/" className="logo">
                 Blog
-            </a>
+            </Link>
             <nav>
-                <a href="">Login</a>
-                <a href="">Register</a>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
             </nav>
         </header>
     );
